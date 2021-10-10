d0dc4e47f7b0cd1712126a7b3d14f7a8fc2f451a
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-10-11 00:31:35 +0600

    icons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -3,6 +3,7 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import Container from '@mui/material/Container';
+import AcUnitOutlinedIcon from '@mui/icons-material/AcUnitOutlined';
 
 export default function Create() {
   return (
@@ -41,8 +42,16 @@ export default function Create() {
        <Button type="submit" color="success" variant="contained" onClick ={ () => console.log('You clicked me') }  >
         Submit
       </Button>
+          <br/>
+      <AcUnitOutlinedIcon/>  
+
+     <AcUnitOutlinedIcon  color="secondary" fontSize="large" />  
+       <AcUnitOutlinedIcon  color="secondary" fontSize="small" /> 
+        <AcUnitOutlinedIcon  color="action" fontSize="small" /> 
+<AcUnitOutlinedIcon  color="error" fontSize="small" /> 
 
 
+<AcUnitOutlinedIcon  color="disabled" fontSize="small" /> 
     </Container>
   );
 }
